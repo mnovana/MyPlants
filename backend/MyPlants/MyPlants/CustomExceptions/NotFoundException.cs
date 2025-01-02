8edcfb5beda8ce952ce0d2beab42bd6c24431e35
mnovana <maravicnovana@gmail.com>
2025-01-02 20:56:16 +0100

    Add NotFoundException

---
+++ backend/MyPlants/MyPlants/CustomExceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace MyPlants.CustomExceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message = "Entity was not found") : base(message) { }
+    }
+}
