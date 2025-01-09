0816eb3085748cf25f4c0da3b5ac0a2caa71a5e4
mnovana <maravicnovana@gmail.com>
2025-01-09 22:07:05 +0100

    Add custom font

--- frontend/MyPlants/src/layouts/Header.jsx
+++ frontend/MyPlants/src/layouts/Header.jsx
@@ -2,13 +2,13 @@ import gardenerImg from "../assets/gardener.png";
 
 function Header() {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between p-10">
       <div>
-        <div className="text-3xl font-bold text-green-900">My Plants</div>
-        <div>Track plants and their watering schedule!</div>
+        <div className="text-6xl font-bold text-green-800">My Plants</div>
+        <div className="text-xl tracking-wide">Track plants and their watering schedule!</div>
       </div>
 
-      <img src={gardenerImg} />
+      <img src={gardenerImg} className="h-28" />
     </div>
   );
 }
--- frontend/MyPlants/tailwind.config.js
+++ frontend/MyPlants/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Manjari", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
